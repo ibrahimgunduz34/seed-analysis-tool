f01ac1b1f71a4ecdf05fbe46e210146a93ba71b9
Ibrahim Gunduz <ibrahimgunduz34@gmail.com>
2025-09-02 13:52:47 +0300

    parameterize the report

--- src/main/java/org/seed/fund/command/GenerateReport.java
+++ src/main/java/org/seed/fund/command/GenerateReport.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 @ConditionalOnProperty(name = "task", havingValue = "GenerateReport")
@@ -21,19 +23,24 @@ public class GenerateReport implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<String> codes = List.of(
-                "BDS",
-                "MAC",
-                "NRC",
-                "KHA",
-                "PPH",
-                "DTL",
-                "KHC",
-                "KVT",
-                "GNS"
-        );
-        LocalDate beginDate = LocalDate.parse("2025-08-05", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        LocalDate endDate = LocalDate.parse("2025-08-29", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//        List<String> codes = List.of(
+//                "BDS",
+//                "PPH",
+//                "DTL",
+//                "KHC",
+//                "KVT",
+//                "GNS"
+//        );
+
+//        LocalDate beginDate = LocalDate.parse("2025-02-29", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+//        LocalDate endDate = LocalDate.parse("2025-09-02", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        LocalDate beginDate = LocalDate.parse(args[0], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate endDate = LocalDate.parse(args[1], DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        List<String> codes = Arrays.stream(args[2].split(","))
+                .map(String::trim).filter(s -> !s.isEmpty()).toList();
+
         BigDecimal initialAmount = BigDecimal.valueOf(10_000);
         Integer frequency = 30;
 
